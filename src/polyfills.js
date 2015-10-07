74c5cfaa0cbd8b9c12eebaab187b81b8ed97905e
Jason Eberle <jason@imgix.com>
2015-10-07 14:43:28 -0400

    Removing the console polyfill

--- src/polyfills.js
+++ src/polyfills.js
@@ -9,25 +9,6 @@
 			};
 		}
 
-		// Console-polyfill. MIT license.
-		// https://github.com/paulmillr/console-polyfill
-		// Make it safe to do console.log() always.
-		(function(con) {
-			var prop, method;
-			var empty = {};
-			var dummy = function() {};
-			var properties = 'memory'.split(',');
-			var methods = ('assert,clear,count,debug,dir,dirxml,error,exception,group,' +
-			'groupCollapsed,groupEnd,info,log,markTimeline,profile,profiles,profileEnd,' +
-			'show,table,time,timeEnd,timeline,timelineEnd,timeStamp,trace,warn').split(',');
-			while (prop = properties.pop()) {
-				con[prop] = con[prop] || empty;
-			}
-			while (method = methods.pop()) {
-				con[method] = con[method] || dummy;
-			}
-		})(window.console || {}); // Using `this` for web workers.
-
 		// mozilla's Function.bind polyfill
 		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind
 		if (!Function.prototype.bind) {
