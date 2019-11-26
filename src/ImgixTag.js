81f3b2dda42754249c0ed5e1efcb17836981491a
Sherwin H <sherwin@imgix.com>
2019-11-26 14:28:11 -0800

    refactor: exit out of flow using empty return statements

--- src/ImgixTag.js
+++ src/ImgixTag.js
@@ -8,54 +8,56 @@ var ImgixTag = (function() {
 
     if (!this.el) {
       console.warn('ImgixTag must be passed a DOM element.');
-    } else {
-      if (this.el.hasAttribute('ix-initialized') && !this.settings.force) {
-        return;
-      }
-
-      this.ixPathVal = el.getAttribute(this.settings.pathInputAttribute);
-      this.ixParamsVal = el.getAttribute(this.settings.paramsInputAttribute);
-      this.ixSrcVal = el.getAttribute(this.settings.srcInputAttribute);
-      this.ixHostVal =
-        el.getAttribute(this.settings.hostInputAttribute) || this.settings.host;
-
-      if (this.ixPathVal && !this.ixHostVal) {
-        console.warn(
-          'You must set a value for `imgix.config.host` or specify an `ix-host` attribute to use `ix-path` and `ix-params`.'
-        );
-      } else if (
-        typeof this.ixPathVal === 'string' &&
-        this.ixPathVal.length == 0
-      ) {
-        console.warn('`ix-path` cannot accept a value of empty string ""');
-      } else if (
-        typeof this.ixSrcVal === 'string' &&
-        this.ixSrcVal.length == 0
-      ) {
-        console.warn('`ix-src` cannot accept a value of empty string ""');
-      } else {
-        this.baseParams = this._extractBaseParams();
-        this.baseUrl = this._buildBaseUrl();
-        this.baseUrlWithoutQuery = this.baseUrl.split('?')[0];
-
-        if (util.isString(this.settings.sizesAttribute)) {
-          this.el.setAttribute(this.settings.sizesAttribute, this.sizes());
-        }
-
-        if (util.isString(this.settings.srcsetAttribute)) {
-          this.el.setAttribute(this.settings.srcsetAttribute, this.srcset());
-        }
-
-        if (
-          util.isString(this.settings.srcAttribute) &&
-          this.el.nodeName == 'IMG'
-        ) {
-          this.el.setAttribute(this.settings.srcAttribute, this.src());
-        }
-
-        this.el.setAttribute('ix-initialized', 'ix-initialized');
-      }
+      return;
     }
+
+    if (this.el.hasAttribute('ix-initialized') && !this.settings.force) {
+      return;
+    }
+
+    this.ixPathVal = el.getAttribute(this.settings.pathInputAttribute);
+    this.ixParamsVal = el.getAttribute(this.settings.paramsInputAttribute);
+    this.ixSrcVal = el.getAttribute(this.settings.srcInputAttribute);
+    this.ixHostVal =
+      el.getAttribute(this.settings.hostInputAttribute) || this.settings.host;
+
+    if (this.ixPathVal && !this.ixHostVal) {
+      console.warn(
+        'You must set a value for `imgix.config.host` or specify an `ix-host` attribute to use `ix-path` and `ix-params`.'
+      );
+      return;
+    }
+
+    if (typeof this.ixPathVal === 'string' && this.ixPathVal.length == 0) {
+      console.warn('`ix-path` cannot accept a value of empty string ""');
+      return;
+    }
+
+    if (typeof this.ixSrcVal === 'string' && this.ixSrcVal.length == 0) {
+      console.warn('`ix-src` cannot accept a value of empty string ""');
+      return;
+    }
+
+    this.baseParams = this._extractBaseParams();
+    this.baseUrl = this._buildBaseUrl();
+    this.baseUrlWithoutQuery = this.baseUrl.split('?')[0];
+
+    if (util.isString(this.settings.sizesAttribute)) {
+      this.el.setAttribute(this.settings.sizesAttribute, this.sizes());
+    }
+
+    if (util.isString(this.settings.srcsetAttribute)) {
+      this.el.setAttribute(this.settings.srcsetAttribute, this.srcset());
+    }
+
+    if (
+      util.isString(this.settings.srcAttribute) &&
+      this.el.nodeName == 'IMG'
+    ) {
+      this.el.setAttribute(this.settings.srcAttribute, this.src());
+    }
+
+    this.el.setAttribute('ix-initialized', 'ix-initialized');
   }
 
   ImgixTag.prototype._extractBaseParams = function() {
