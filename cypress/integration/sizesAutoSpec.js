21c8535e2113d8442ac23d293cf44adf9c5923b6
Luis Ball <lball@imgix.com>
2021-05-06 12:28:17 -0400

    test(sizes): ensure module waits for debounce before setting sizes
    Co-authored-by: Frederick Fogerty <frederick@imgix.com>

--- cypress/integration/sizesAutoSpec.js
+++ cypress/integration/sizesAutoSpec.js
@@ -19,11 +19,11 @@ describe('On a pages first render', () => {
 describe('When a page gets resized', () => {
   before(() => {
     cy.visit('/cypress/fixtures/samplePage.html');
+    cy.viewport(500, 500);
   });
 
   beforeEach(() => {
     cy.fixture('config.js').as('config');
-    cy.viewport(500, 500);
   });
 
   it('Updates the sizes attribute on resize', () => {
@@ -35,31 +35,37 @@ describe('When a page gets resized', () => {
     });
   });
 
-  it(`Check resize waits for debounce`, () => {
-    // resize browser to x
-    // check sizes hasn't changed to x
-    // resize browser to y
-    // check sizes hasn't changed to x
-    // wait 200ms
-    // check sizes has changed to y
-    cy.visit('/cypress/fixtures/samplePage.html');
-    cy.viewport(500, 500);
-    cy.get('[data-test-id="sizes"]')
-      .first()
-      .then(($el) => {
-        // const expectedSize = Math.ceil($el.width());
-        const imgSize = Number($el.attr('sizes').split('px')[0]);
-        assert.isAtMost(imgSize, 500);
-        cy.viewport(1000, 500);
-        cy.wait(10);
-        // check sizes HASN'T changed
-        cy.viewport(1500, 500);
-        cy.wait(200);
-        // check sizes HAS changed
-        const newImageSize = Number($el.attr('sizes').split('px')[0]);
-        expect(newImageSize).to.not.equal(imgSize);
-        // assert.isAtLeast(imgSize, expectedSize);
-      });
+  context('When multiple resizes happen', () => {
+    it(`Does not update size before debounce is finished`, () => {
+      cy.get('[data-test-id="sizes"]')
+        .first()
+        .then(($el) => {
+          // store and check the image size before resize
+          let imgSize = Number($el.attr('sizes').split('px')[0]);
+          const expectedSize = Math.ceil($el.width());
+          assert.isAtMost(imgSize, 500);
+          // resize and wait only 10ms
+          cy.viewport(1000, 500);
+          cy.wait(10);
+          // check sizes has NOT changed
+          imgSize = Number($el.attr('sizes').split('px')[0]);
+          assert.isAtMost(imgSize, 500);
+          assert.isAtLeast(imgSize, expectedSize);
+        });
+    });
+    it('Updates size after debounce has finished', () => {
+      cy.viewport(1500, 500);
+      cy.wait(300);
+      cy.get('[data-test-id="sizes"]')
+        .first()
+        .then(($el) => {
+          // check sizes HAS changed
+          const newImgSize = Number($el.attr('sizes').split('px')[0]);
+          assert.isAtLeast(newImgSize, 500);
+          assert.isAtMost(newImgSize, 1500);
+          expect(newImgSize).to.not.equal(404);
+        });
+    });
   });
 });
 
