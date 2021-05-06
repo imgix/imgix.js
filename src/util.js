c497a70da40486cba398f9f7cf89173eafa09045
Luis Ball <lball@imgix.com>
2021-05-06 10:32:16 -0400

    chore(debounce): add debounce fn and refactor raf
    Co-authored-by: Frederick Fogerty <frederick@imgix.com>

--- src/util.js
+++ src/util.js
@@ -102,4 +102,38 @@ module.exports = {
       return metaTagContent;
     }
   },
+  debounce: function (func, waitMs) {
+    // based off: http://modernjavascript.blogspot.com/2013/08/building-better-debounce.html
+    // we need to save these in the closure
+    var timeout, args, context, timestamp;
+
+    return function () {
+      // save details of latest call
+      context = this;
+      args = [].slice.call(arguments, 0);
+      timestamp = new Date();
+
+      // this is where the magic happens
+      var later = function () {
+        // how long ago was the last call
+        var timeSinceLastCallMs = new Date() - timestamp;
+
+        // if the latest call was less that the wait period ago
+        // then we reset the timeout to wait for the difference
+        if (timeSinceLastCallMs < waitMs) {
+          timeout = setTimeout(later, waitMs - timeSinceLastCallMs);
+
+          // or if not we can null out the timer and run the latest
+        } else {
+          timeout = null;
+          func.apply(context, args);
+        }
+      };
+
+      // we only need to set the timer now if one isn't already running
+      if (!timeout) {
+        timeout = setTimeout(later, waitMs);
+      }
+    };
+  },
 };
