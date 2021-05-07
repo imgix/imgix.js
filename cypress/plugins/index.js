34c134dab87e36d54cbc493d93298214c4fa2f63
Luis Ball <lball@imgix.com>
2021-05-07 16:04:38 -0400

    test(cypress): add cy-audit for lighthouse tests

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,5 +1,15 @@
-module.exports = (on) => {
+const { lighthouse, pa11y, prepareAudit } = require('cypress-audit');
+
+module.exports = (on, config) => {
   require('cypress-terminal-report/src/installLogsPrinter')(on, {
     printLogsToConsole: 'onFail',
+  });
+  on('before:browser:launch', (browser = {}, launchOptions) => {
+    prepareAudit(launchOptions);
+  });
+
+  on('task', {
+    lighthouse: lighthouse(), // calling the function is important
+    pa11y: pa11y(), // calling the function is important
   });
 };
