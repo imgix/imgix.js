6049d8d6a0e2785eaffe72497c3803ca76013264
Luis Ball <lball@imgix.com>
2021-05-10 13:55:40 -0400

    chore: create sizes auto example in index.html

---
+++ index.js
@@ -0,0 +1,23 @@
+const images = document.querySelectorAll('#photos-grid img');
+
+console.log(images);
+
+images.forEach((img) => {
+  const newDiv = document.createElement('div');
+  const newP = document.createElement('p');
+  let size = img.getAttribute('sizes');
+  newDiv.className = 'size-info';
+  newP.textContent = 'sizes=' + size;
+  newDiv.appendChild(newP);
+  img.insertAdjacentElement('afterend', newDiv);
+  console.log(img, newDiv);
+});
+
+window.addEventListener('resize', () => {
+  images.forEach((img) => {
+    let div = img.nextSibling;
+    let text = div.firstChild;
+    let size = img.getAttribute('sizes');
+    text.textContent = 'sizes=' + size;
+  });
+});
