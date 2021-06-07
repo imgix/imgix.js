a8c6e1053d9bc1013a8f7d988bb6c7a0918d8491
Luis Ball <lball@imgix.com>
2021-06-07 16:56:48 -0400

    fix: bump version to match current
    index.js had not been updated to relfect the currently released version
    which was affecting out internal reporting for this version of the
    library. This commit fixes the version number.

--- src/imgix.js
+++ src/imgix.js
@@ -2,7 +2,7 @@ var ImgixTag = require('./ImgixTag.js'),
   util = require('./util.js'),
   defaultConfig = require('./defaultConfig');
 
-var VERSION = '3.4.2';
+var VERSION = '3.5.0';
 
 global.imgix = {
   init: function (opts) {
