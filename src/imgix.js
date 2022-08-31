6385ec40a584090a9fba41bc1417dc365b120937
imgix-git-robot <support@imgix.com>
2022-08-31 17:30:38 +0000

    chore(release): 3.6.1-rc.1
    ## [3.6.1-rc.1](https://github.com/imgix/imgix.js/compare/v3.6.0...v3.6.1-rc.1) (2022-08-31)
    
    ### Bug Fixes
    
    * **ci:** add prepare step to publish ([1804dce](https://github.com/imgix/imgix.js/commit/1804dce807aeab689a1d326e2f3c07bad3cff48d))
    * **deploy:** corrected regex mismatch in semantic release ([914c091](https://github.com/imgix/imgix.js/commit/914c0915db6790a75129502bdadac14e95e2b360))
    
     [skip ci]

--- src/imgix.js
+++ src/imgix.js
@@ -2,7 +2,7 @@ var ImgixTag = require('./ImgixTag.js'),
   util = require('./util.js'),
   defaultConfig = require('./defaultConfig');
 
-var VERSION = '3.6.0';
+var VERSION = '3.6.1-rc.1';
 
 global.imgix = {
   init: function (opts) {
